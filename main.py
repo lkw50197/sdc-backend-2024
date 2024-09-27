70cae2824c16393dfd4536ba6fb6e29bce6c387a
lkw50197 <lkw50197@gmail.com>
2024-09-27 20:07:22 +0800

    add lkw50197 hw2

--- main.py
+++ main.py
@@ -6,3 +6,10 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: Item, q: str | None = None):
+    result = {"item_id": item_id, **item.model_dump()}
+    if q:
+        result.update({"q": q})
+    return result
